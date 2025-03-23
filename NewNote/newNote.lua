87d1aec2c4801bb740cd58106828c26636207c2e
Digi <marton@szabomarci.hu>
2025-03-23 08:32:58 +0100

    added NewNote module, which makes easier to create a new note to an already existing README in current folder, requires Notes dir

---
+++ NewNote/newNote.lua
@@ -0,0 +1,27 @@
+-- Capture all arguments passed to the function
+local args = {...}
+    
+-- If no arguments are provided, default to "NewNote"
+if #args == 0 then
+    table.insert(args, "NewNote")
+end
+
+-- Join all arguments with underscores
+local baseName = table.concat(args, "_")
+
+function NewFileNameGenerator(...)
+    return baseName .. "_" .. os.date("%Y-%m-%d_%H-%M-%S") .. ".md"
+end
+
+local fileName = "Notes\\" .. NewFileNameGenerator(...)
+
+local createNewFile = "powershell -Command mdh " .. fileName .. " h 1 " .. string.gsub(baseName, "_", " ")
+os.execute(createNewFile)
+
+local defaultNoteFile = "README.md"
+
+local linkNewFile = "powershell -Command mdh " .. defaultNoteFile .. " link " .. fileName .. " " .. baseName
+os.execute(linkNewFile)
+
+local openeditor = "nvim " .. fileName
+os.execute(openeditor)
